185a7a5515bfa92de6a37665eb050b4571b97212
unknown <2359725483@qq..com>
2023-07-02 02:21:22 +0800

    fix conflict

--- modules/segmentation/segmentation.py
+++ modules/segmentation/segmentation.py
@@ -1,10 +1,30 @@
+from ultralytics import SAM
+from ...models.fastsam import FastSAM 
 
 
 class FoodSegmentation():
 
     def __init__(self, config, device):
         self.device = device
+        self.model = SAM('sam_b.pt')
        
     def __call__(self, img):
-        
-        return None
+        results = self.model(img)
+        return results
+
+class FoodSegmentationFastSAM():
+
+    def __init__(self, config, device):
+        self.device = device
+        self.model = FastSAM(args.model_path)
+       
+    def __call__(self, img):
+        results = self.model(
+            img,
+            device=self.device,
+            retina_masks=True,
+            imgsz=1024,
+            conf=0.4,
+            iou=0.9, 
+        )
+        return results
